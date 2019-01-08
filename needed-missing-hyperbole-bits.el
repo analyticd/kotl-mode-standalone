d4e62a329f9cdbe8c116dc1b18852ee40d6cd3a1
Foo Bar <4829djaskdfj@gmail.com>
2019-01-08 13:15:52 -0800

    Add mail tree feature supporting code from hyperbole

--- needed-missing-hyperbole-bits.el
+++ needed-missing-hyperbole-bits.el
@@ -163,3 +163,197 @@ that returns a replacement string."
                              (char-to-string c))))
                        newtext ""))))))
     (concat rtn-str (substring str start))))
+
+;;; From hargs.el, needed for mail tree
+(defvar hargs:reading-p nil
+  "t only when Hyperbole is prompting user for input, else nil.")
+
+(defun hattr:get (obj-symbol attr-symbol)
+  "Returns value of OBJ-SYMBOL's attribute ATTR-SYMBOL."
+  (get obj-symbol attr-symbol))
+
+(defvar hbut:current nil
+  "The currently selected Hyperbole button. Available to action routines.")
+
+(defun hargs:select-event-window ()
+  "Select window, if any, that mouse was over during last event."
+  (if (featurep 'xemacs)
+      (if current-mouse-event
+	  (select-window
+	   (or (event-window current-mouse-event)
+	       (selected-window))))
+    (let* ((event last-command-event)
+	   (window (posn-window (event-start event))))
+      (if (and (eq window (minibuffer-window))
+	       (not (minibuffer-window-active-p
+		     (minibuffer-window))))
+	  (error "Attempt to select inactive minibuffer window")
+	(select-window (or window (selected-window)))))))
+
+(defun hargs:read (prompt &optional predicate default err val-type)
+  "PROMPTs without completion for a value matching PREDICATE and returns it.
+PREDICATE is an optional boolean function of one argument.  Optional DEFAULT
+is a string to insert after PROMPT as the default return value.  Optional
+ERR is a string to display temporarily when an invalid value is given.
+Optional VAL-TYPE is a symbol indicating the type of value to be read.  If
+VAL-TYPE equals `sexpression', then return that type; otherwise return the
+string read or nil."
+  (let ((bad-val) (val) (stringify)
+	      (prev-reading-p hargs:reading-p) (read-func)
+	      (owind (selected-window))
+	      (obuf (current-buffer)))
+    (unwind-protect
+	      (progn
+	        (cond ((or (null val-type) (eq val-type 'sexpression))
+		             (setq read-func 'read-minibuffer
+		                   hargs:reading-p 'sexpression))
+		            (t (setq read-func 'read-string hargs:reading-p val-type
+			                   stringify t)))
+	        (while (progn (and default (not (stringp default))
+			                       (setq default (prin1-to-string default)))
+			                  (condition-case ()
+			                      (or bad-val (setq val (funcall read-func prompt default)))
+			                    (error (setq bad-val t)))
+			                  (if bad-val
+			                      t
+			                    (and stringify
+			                         ;; Remove any double quoting of strings.
+			                         (string-match "\\`\"\\([^\"]*\\)\"\\'" val)
+			                         (setq val (match-string 1 val)))
+			                    (and predicate (not (funcall predicate val)))))
+	          (if bad-val (setq bad-val nil) (setq default val))
+	          (beep)
+	          (if err (progn (message err) (sit-for 3))))
+	        val)
+      (setq hargs:reading-p prev-reading-p)
+      (select-window owind)
+      (switch-to-buffer obuf))))
+
+(defun hmail:invoke (&optional address cc subject)
+  "Invoke user preferred mail composer: vm-mail, mh-send or mail.
+Optional arguments are ADDRESS, CC list and SUBJECT of mail."
+  ;; Next 3 lines prevent blank lines between fields due to
+  ;; fill-region-as-paragraph within mail-setup.
+  (if (equal address "") (setq address nil))
+  (if (equal cc "") (setq cc nil))
+  (if (equal subject "") (setq subject nil))
+  (compose-mail address subject (if cc (list (cons "CC" cc)))))
+
+(defun hypb:insert-region (buffer start end invisible-flag)
+  "Insert into BUFFER the contents of the region from START to END within the current buffer.
+INVISIBLE-FLAG, if non-nil, means invisible text in an outline region is
+copied, otherwise, it is omitted."
+  (if invisible-flag
+      ;; Skip hidden blank lines between cells but include hidden outline text.
+      (while (< start end)
+	      (if (not (get-text-property start 'invisible))
+	          (append-to-buffer buffer start (1+ start)))
+	      (setq start (1+ start)))
+    ;; Skip both hidden blank lines between cells and hidden outline text.
+    (while (< start end)
+      (or (kview:char-invisible-p start) (append-to-buffer buffer start (1+ start)))
+      (setq start (1+ start)))))
+
+(defun hmail:region (start end &optional buf invisible-flag)
+  "Start composing mail with region between START and END included in message.
+Invisible text is expanded and included in the mail only if INVISIBLE-FLAG is
+non-nil.  Optional BUF contains the region and defaults to the current
+buffer.  It may be a buffer or buffer name."
+  (interactive (list (region-beginning) (region-end) (current-buffer)
+		                 (y-or-n-p "Include invisible text? ")))
+  (or buf (setq buf (current-buffer)))
+  (if (stringp buf) (setq buf (get-buffer buf)))
+  (let (mail-buf)
+    (hmail:invoke)
+    (setq mail-buf (current-buffer))
+    (save-excursion
+      (if (search-forward mail-header-separator nil t)
+	        ;; Within header, so move to body
+	        (goto-char (point-max)))
+      (set-buffer buf)
+      (hypb:insert-region mail-buf start end invisible-flag))))
+
+(defun hargs:iform-read (iform &optional modifying)
+  "Reads action arguments according to IFORM, a list with car = 'interactive.
+Optional MODIFYING non-nil indicates current button is being modified, so
+button's current values should be presented as defaults.  Otherwise, uses
+hargs:defaults as list of defaults, if any.
+See also documentation for `interactive'."
+  ;; This is mostly a translation of `call-interactively' to Lisp.
+  ;;
+  ;; Save this now, since use of minibuffer will clobber it.
+  (setq prefix-arg current-prefix-arg)
+  (if (not (and (listp iform) (eq (car iform) 'interactive)))
+      (error
+       "(hargs:iform-read): arg must be a list whose car = 'interactive.")
+    (setq iform (car (cdr iform)))
+    (if (or (null iform) (and (stringp iform) (equal iform "")))
+	      nil
+      (let ((prev-reading-p hargs:reading-p))
+	      (unwind-protect
+	          (progn
+	            (setq hargs:reading-p t)
+	            (if (not (stringp iform))
+		              (let ((defaults (if modifying
+				                              (hattr:get 'hbut:current 'args)
+				                            (and (boundp 'hargs:defaults)
+					                               (listp hargs:defaults)
+					                               hargs:defaults)
+				                            )))
+		                (eval iform))
+		            (let ((i 0) (start 0) (end (length iform))
+		                  (ientry) (results) (val) (default)
+		                  (defaults (if modifying
+				                            (hattr:get 'hbut:current 'args)
+				                          (and (boundp 'hargs:defaults)
+				                               (listp hargs:defaults)
+				                               hargs:defaults)
+				                          )))
+		              ;;
+		              ;; Handle special initial interactive string chars.
+		              ;;
+		              ;;   `*' means error if buffer is read-only.
+		              ;;   Notion of when action cannot be performed due to
+		              ;;   read-only buffer is view-specific, so here, we just
+		              ;;   ignore a read-only specification since it is checked for
+		              ;;   earlier by any ebut edit code.
+		              ;;
+		              ;;   `@' means select window of last mouse event.
+		              ;;
+		              ;;   `^' means activate/deactivate mark depending on invocation thru shift translation
+		              ;;   See `this-command-keys-shift-translated' for somewhat of an explanation.
+		              ;;
+		              ;;   `_' means keep region in same state (active or inactive)
+		              ;;   after this command.  (XEmacs only.)
+		              ;;
+		              (while (cond 
+			                    ((eq (aref iform i) ?*))
+			                    ((eq (aref iform i) ?@)
+			                     (hargs:select-event-window)
+			                     t)
+			                    ((eq (aref iform i) ?^)
+			                     (handle-shift-selection))
+			                    ((eq (aref iform i) ?_)
+			                     (setq zmacs-region-stays t)))
+		                (setq i (1+ i) start i))
+		              ;;
+		              (while (and (< start end)
+			                        (string-match "\n\\|\\'" iform start))
+		                (setq start (match-end 0)
+			                    ientry (substring iform i (match-beginning 0))
+			                    i start
+			                    default (car defaults)
+			                    default (if (or (null default) (stringp default))
+				                              default
+				                            (prin1-to-string default))
+			                    val (hargs:get ientry default (car results))
+			                    defaults (cdr defaults)
+			                    results (cond ((or (null val) (not (listp val)))
+					                               (cons val results))
+					                              ;; Is a list of args?
+					                              ((eq (car val) 'args)
+					                               (append (nreverse (cdr val)) results))
+					                              (t ;; regular list value
+					                               (cons val results)))))
+		              (nreverse results))))
+	        (setq hargs:reading-p prev-reading-p))))))
